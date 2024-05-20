745f4d13012d1bb0dddc77110ccee3ce9c524d0a
Shahla <Ushamdeenshahla@gmail.com>
2024-05-20 15:49:23 +0530

    my first commit

---
+++ src/Animal.java
@@ -0,0 +1,28 @@
+
+public class Animal {
+
+    String name;
+
+    String type;
+
+    String sound;
+
+
+
+    Animal(String name,String type, String sound){
+
+        this.name=name;
+        this.type=type;
+        this.sound=sound;
+    }
+
+    void attack () {
+        System.out.println(name + " can eat");
+    }
+    void defend () {
+        System.out.println(name + " can jump");
+    }
+    void dodge () {
+        System.out.println(name + " can play");
+    }
+}
